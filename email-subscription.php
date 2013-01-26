e72e8eb860984023e846b41464b2992985372d01
Ryan Bagwell <ryan@ryanbagwell.com>
2013-01-26 17:23:19 -0600

    making it php 5.2 compatible

--- email-subscription.php
+++ email-subscription.php
@@ -16,15 +16,15 @@ class EmailSignup extends WP_Widget  {
 	public function EmailSignup() {
 	
 		wp_register_script('jquery-validate', 
-			plugins_url( basename(__DIR__) . '/js/jquery.validate.js' ), 
+			plugins_url( basename( dirname(__FILE__) )  . '/js/jquery.validate.js' ), 
 			    'jquery', '1.0', true );
 
 		wp_register_script('email-subscription', 
-			plugins_url( basename(__DIR__) .'/js/email-subscription.js' ), 
+			plugins_url( basename( dirname(__FILE__) ) .'/js/email-subscription.js' ), 
 			    'jquery-validate', '1.0', true );
 
 		wp_register_script('jquery-infield-label', 
-			plugins_url( basename(__DIR__) . '/js/jquery.infieldlabel.min.js' ), 
+			plugins_url( basename( dirname(__FILE__) ) . '/js/jquery.infieldlabel.min.js' ), 
 			    'jquery', '1.0', false );
 
 		add_action( 'wp_ajax_nopriv_email_signup', 
@@ -51,6 +51,9 @@ class EmailSignup extends WP_Widget  {
 
 	}
 
+    /*
+     * Initializes the widget
+     */
 	public function start_widget() {
 		register_widget( 'EmailSignupWidget' );
     }
@@ -60,7 +63,7 @@ class EmailSignup extends WP_Widget  {
 	 * Prints the HTML signup form to the page
 	 */
 	public function show_signup_form() {
-		require_once(dirname(__FILE__).'/form.tpl.php');
+		require_once( dirname(__FILE__).'/form.tpl.php' );
 	}
 
 
@@ -70,7 +73,7 @@ class EmailSignup extends WP_Widget  {
 	public function print_scripts() {
 		echo "<script type='text/javascript'>var ajaxURL='".admin_url('admin-ajax.php')."';</script>";
         
-        //this can be uncommented later when we get the require.js stuff figured out		
+        //this can be uncommented later when we get the require.js stuff figured out
         // wp_enqueue_script( 'jquery' );
         // wp_enqueue_script( 'jquery-validate' );
         // wp_enqueue_script('email-subscription');
@@ -83,24 +86,35 @@ class EmailSignup extends WP_Widget  {
 		wp_enqueue_script('jquery-infield-label');
 	}	
 
+	
+	/*
+	 * Calls the signup() method on the selected third-party service, which
+	 * sends the data to that service
+	 */
 	public function submit_email() {
 	    $service = $this->get_selected_service();
 	    $response = $service->signup();
-		die('email submitted');
 	}
 	
+	/*
+	 * The method that adds the admin menu item
+	 */
 	public function add_settings() {
 		add_options_page( 'Email Signup Options', 'Email Signups', 
 			'manage_options', 'email-signups.php', array($this, 'options_html') );
 	}
 
+	/*
+	 * The method that adds the admin html page
+	 */
 	public function options_html() {
 
 	    if ( array_key_exists( 'selected_submit_handler', $_POST) )
 	        update_option( 'selected_submit_handler', $_POST['selected_submit_handler']);
 	        
 	    if ( array_key_exists( 'email_signup_default_widget_title', $_POST) )
-	        update_option( 'email_signup_default_widget_title', $_POST['email_signup_default_widget_title']);	        
+	        update_option( 'email_signup_default_widget_title', $_POST['email_signup_default_widget_title']);
+	        
 		require_once('lib/php-form-helpers/form.class.php');
 		require_once('templates/admin/admin.tpl.php');
 	}
@@ -124,6 +138,9 @@ class EmailSignup extends WP_Widget  {
         }
 	}
 	
+	/*
+	 * Returns the selected third-party service
+	 */
 	public function get_selected_service() {
 	    $this->load_services();
 	    $selected_service = get_option( 'selected_submit_handler', false );
